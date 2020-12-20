2fcd45fcd61ad293bbf2231bbf8ebb6f909825d1
jb_c <>
2020-12-20 23:54:56 +0000

    some alpha analysis

---
+++ 2. Model exploration/Pickle Converter.py
@@ -0,0 +1,6 @@
+import pickle
+import pandas as pd
+
+df = pd.read_pickle(r'K:\Code\Tennis_Betting\2. Model exploration\alphas\mens_alphas_15.0.pickle')
+with open('alphas/mens_alphas.pickle', 'wb') as handle:
+    pickle.dump(df, handle)
